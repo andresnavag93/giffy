9c653a8ef4d1597bbe89f03467ae5dee7a19d475
Andres Navarro <andresnavag93@gmail.com>
2020-11-23 15:33:20 -0400

    Learning of infinite scroll, css grid and usecallback

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+import { render, waitForElement, fireEvent, screen } from "@testing-library/react";
+
+test("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
+
+test("home works as expected", async () => {
+  const { container } = render(<App />);
+  const gifLink = await waitForElement(() => container.querySelector(".Gif-link"));
+  expect(gifLink).toBeVisible();
+});
+
+test("search form could be used", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.change(screen.getByRole("textbox"), { target: { value: "Matrix" } });
+  fireEvent.click(screen.getByRole("button"));
+  const title = await screen.findByText("Matrix");
+  expect(title).toBeVisible();
 });
--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,13 +1,31 @@
-import React from "react";
-import ListOfGifs from "components/ListOfGifs/ListOfGifs";
+import React, { useEffect, useRef, useCallback } from "react";
 import Spinner from "components/Spinner/Spinner";
+import ListOfGifs from "components/ListOfGifs/ListOfGifs";
 import { useGifs } from "hooks/useGifs";
+import useNearScreen from "hooks/useNearScreen";
+import debounce from "just-debounce-it";
 
 export default function SearchResults({ params }) {
   const { keyword } = params;
   const { loading, gifs, setPage } = useGifs({ keyword });
+  const visorRef = useRef();
+  const { isNearScreen } = useNearScreen({ externalRef: !loading && visorRef, once: false });
 
-  const handleNextPage = () => setPage((prevPage) => prevPage + 1);
+  console.log(isNearScreen);
+
+  // const debounceNextPage = useCallback(debounce(
+  //   () => console.log('next page'), 2000
+  // ))
+
+  const debounceNextPage = useCallback(
+    debounce(() => setPage((prevPage) => prevPage + 1), 2000),
+    []
+  );
+
+  useEffect(function () {
+    // if (isNearScreen) setPage(prevPage => prevPage + 1)
+    if (isNearScreen) debounceNextPage();
+  });
 
   return (
     <>
@@ -17,10 +35,9 @@ export default function SearchResults({ params }) {
         <>
           <h3 className="App-title">{decodeURI(keyword)}</h3>
           <ListOfGifs gifs={gifs} />
+          <div data-testid="visor" ref={visorRef} />
         </>
       )}
-      <br />
-      <button onClick={handleNextPage}>Get next page</button>
     </>
   );
 }
